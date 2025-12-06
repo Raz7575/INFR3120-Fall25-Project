e82341f0412ee6c59ca428fa1a0a7f2202b08041
Raphael Bekele <raphaelbekele@Raphaels-MacBook-Air.local>
2025-12-05 21:39:18 -0500

    Add Angular frontend project

--- app.js
+++ app.js
@@ -1,85 +1,141 @@
-require("dotenv").config();
 
 
-const express = require('express');
-const path = require('path');
-const methodOverride = require('method-override');
-const mongoose = require('mongoose');
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const bcrypt = require("bcryptjs");
+
+const Issue = require("./models/Issue");
+const User = require("./models/User");
 
 const app = express();
-const PORT = 3000;
 
 
-app.use(express.urlencoded({ extended: true })); // Parse form data
-app.use(methodOverride('_method'));              // Support PUT & DELETE from forms
-app.use(express.static(path.join(__dirname, 'public')));
+mongoose
+  .connect("mongodb://127.0.0.1:27017/campusIssues")
+  .then(() => {
+    console.log("MongoDB connected!");
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error:", err);
+  });
 
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/campusIssues")
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+app.use(
+  session({
+    secret: "super-secret-session-key-change-me", 
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: "mongodb://127.0.0.1:27017/campusIssues"
+    }),
+    cookie: {
+      maxAge: 1000 * 60 * 60 
+    }
+  })
+);
 
-
-const issueSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  location: String,
-  priority: String,
-  status: { type: String, default: 'Open' },
-  createdAt: { type: Date, default: Date.now }
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
 });
 
-const Issue = mongoose.model('Issue', issueSchema);
+function requireLogin(req, res, next) {
+  if (!req.session.userId) {
+    return res.redirect("/login");
+  }
+  next();
+}
 
-app.get('/', async (req, res) => {
-  const issues = await Issue.find({});
-  res.render('index', { issues });
+
+app.get("/", async (req, res) => {
+  const issues = await Issue.find().sort({ createdAt: -1 });
+  res.render("index", { issues });
 });
 
-
-app.get('/issues/create', (req, res) => {
-  res.render('create');
+app.get("/issues/create", (req, res) => {
+  res.render("create");
 });
 
-
-app.post('/issues', async (req, res) => {
-  const { title, description, location, priority } = req.body;
-  const newIssue = new Issue({ title, description, location, priority });
-  await newIssue.save();
-  res.redirect('/');
+app.post("/issues", async (req, res) => {
+  await Issue.create(req.body);
+  res.redirect("/");
 });
 
-app.get('/issues/:id', async (req, res) => {
+app.get("/issues/:id", async (req, res) => {
   const issue = await Issue.findById(req.params.id);
-  if (!issue) return res.send('Issue not found');
-  res.render('show', { issue });
+  res.render("show", { issue });
 });
 
-
-app.get('/issues/:id/edit', async (req, res) => {
+app.get("/issues/:id/edit", requireLogin, async (req, res) => {
   const issue = await Issue.findById(req.params.id);
-  if (!issue) return res.send('Issue not found');
-  res.render('edit', { issue });
+  res.render("edit", { issue });
 });
 
-
-app.put('/issues/:id', async (req, res) => {
-  const { title, description, location, priority, status } = req.body;
-  await Issue.findByIdAndUpdate(req.params.id, { title, description, location, priority, status });
-  res.redirect('/');
+app.put("/issues/:id", requireLogin, async (req, res) => {
+  await Issue.findByIdAndUpdate(req.params.id, req.body);
+  res.redirect("/");
 });
 
-
-app.delete('/issues/:id', async (req, res) => {
+app.delete("/issues/:id", requireLogin, async (req, res) => {
   await Issue.findByIdAndDelete(req.params.id);
-  res.redirect('/');
+  res.redirect("/");
 });
 
 
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  const hashed = await bcrypt.hash(password, 10);
+
+  await User.create({ username, password: hashed });
+
+  res.redirect("/login");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.send("Invalid username or password");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    return res.send("Invalid username or password");
+  }
+
+  req.session.userId = user._id;
+  res.redirect("/");
+});
+
+app.get("/logout", (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/login");
+  });
+});
+
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
---
+++ ot-track-frontend/src/app/pages/issue-view/issue-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-issue-view',
+  imports: [],
+  templateUrl: './issue-view.component.html',
+  styleUrl: './issue-view.component.css'
+})
+export class IssueViewComponent {
+
+}
